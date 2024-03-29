21c1b627eaeb60032d85d12fca5bac9ec2654907
Mike Patnode <mike@mpsharp.com>
2024-03-29 08:00:36 -0700

    Simplify

--- challenge/cpp/toh.cpp
+++ challenge/cpp/toh.cpp
@@ -6,26 +6,22 @@ int report;
 void shuffle(int plates, int from, int to, int use) {
 
     if (plates == 1) {
-        if (--report == 0) {
-            printf("%d %d\n", from, to);
-        }
+        printf("move %d %d\n", from, to);
         return;
     }
 
     shuffle(plates - 1, from, use, to);
-    if (--report == 0) {
-        printf("%d %d\n", from, to);
-    }
+    printf("move %d %d\n", from, to);
+
 
     shuffle(plates - 1, use, to, from);
 }
 
 int main() {
-	int tests, plates;
-	scanf("%d\n", &tests);
-	for (int i = 0; i < tests; i++) {
-	    scanf("%d %d\n", &plates, &report);
-	    shuffle(plates, 1, 3, 2);
-	}
+	int plates;
+    printf("Number of plates: ");
+    scanf("%d", &plates);
+    printf("\n");
+    shuffle(plates, 1, 3, 2);
 	return 0;
 }
